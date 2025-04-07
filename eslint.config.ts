315b5749aaf775a02f0138eb95554a5d0f06224e
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2025-04-08 00:11:58 +0700

    fix(dep): fix dependence

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,30 @@
-// @ts-check
-import antfu from '@antfu/eslint-config'
+import tseslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
 
-export default antfu({
-  type: 'lib',
-  pnpm: true,
-})
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    rules: {
+      ...prettier.rules,
+    },
+  },
+];
