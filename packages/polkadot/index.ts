a0841acb8ac2adf5fe65b48cb663ba49edd0603d
CocDap <hodinhdung1997@gmail.com>
2025-04-21 00:00:40 +0700

    feat: make polkadot as package

--- packages/polkadot/index.ts
+++
@@ -1,5 +0,0 @@
-// Polkadot Module exports
-export * from "./chain";
-export { ApiConnection } from "../common/src/types/typeAgent";
-export { CHAINS, XcmTransferParams } from "../common/src/types/xcmTypes";
-export * from "../common/src/types/connect"; 
